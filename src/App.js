7a2ba0e1419f01a0cff81a2c28a8a49e81c76891
ahirhasmukh <ahirhasmukh08@outlook.com>
2019-10-30 21:56:15 +0530

    useEffect data

--- src/App.js
+++ src/App.js
@@ -1,103 +1,43 @@
 import React, { useState, useEffect, useRef } from "react";
 import "./App.scss";
-import Input from "./components/input";
+
+const initXY = {
+  x: null,
+  y: null
+}
 
 function App() {
-  const [name, setName] = useState("test");
-  const [income, setIncome] = useState("high");
+  const [time, setTime] = useState(Date);
+  const [xy, setXY] = useState(initXY);
 
-  const nameRef = useRef(null);
-  const ageRef = useRef(null);
-  const marriedRef = useRef(null);
-  const submitRef = useRef(null);
+  useEffect(()=> {
+    let handle = setInterval(()=> {
+      setTime(Date);
+    }, 3000);
 
-  useEffect(() => {
-    nameRef.current.focus();
-  }, []);
-
-  function keyPressHandler(e) {
-    if (e.keyCode === 13) {
-      if (e.target.id === "nameInput") {
-        ageRef.current.focus();
-      }
-      if (e.target.id === "ageInput") {
-        marriedRef.current.focus();
-      }
-      if (e.target.id === "marriedInput") {
-        submitRef.current.focus();
-      }
+    return () => {
+      clearInterval(handle);
     }
+  })
+
+  function mouseMoveHandler(e) {
+    setXY({
+      x: e.clientX,
+      y: e.clientY
+    })
   }
 
-  function onClickHandle(e) {
-    console.log(e.target);
-  }
-
-  function handleNameChange(e) {
-    setName(e.target.value);
-  }
-
-  function handleIncomeChange(e) {
-    setIncome(e.target.value);
-  }
-
-  function onSubmitHandle(e) {
-    console.log("State =>", name, income);
-    e.preventDefault();
-  }
-
-  const inputStyle = {
-    width: '400px',
-    height: '40px',
-    border: '1px solid #ccc',
-    outline: 0
-  }
+  useEffect(()=> {
+    window.addEventListener("mousemove", mouseMoveHandler);
+    return () => {
+      window.addEventListener("mousemove", mouseMoveHandler);
+    }
+  });
 
   return (
     <div className="App">
-      <form onSubmit={onSubmitHandle}>
-        <div>
-          <label>Name:</label>
-          <input
-            value={name}
-            type="text"
-            onChange={handleNameChange}
-            placeholder="Enter name"
-          />
-        </div>
-
-        <div>
-          <label>Income:</label>
-          <select value={income} onChange={handleIncomeChange}>
-            <option value="high">High</option>
-            <option value="medium">Medium</option>
-            <option value="low">Low</option>
-          </select>
-        </div>
-
-        <div>
-          <button type="submit">Save</button>
-        </div>
-      </form>
-
-      <div>
-        <label>Name:</label>
-        <Input ref={nameRef} id="nameInput" type="text" placeholder="Enter name" style={inputStyle} onKeyDown={keyPressHandler}></Input>
-      </div>
-
-      <div>
-        <label>Age:</label>
-        <Input ref={ageRef} id="ageInput" type="text" placeholder="Enter age" style={inputStyle} onKeyDown={keyPressHandler}></Input>
-      </div>
-
-      <div>
-        <label>Married:</label>
-        <Input ref={marriedRef} id="marriedInput" type="checkbox" onKeyDown={keyPressHandler}></Input>
-      </div>
-
-      <div>
-        <button ref={submitRef} onClick={onClickHandle} type="submit">Submit</button>
-      </div>
+      <span style={{margin:"20px",display:"inline-block"}}>Date & Time: {time}</span>
+      <h3>{`x: ${xy.x}, y: ${xy.y}`}</h3>
     </div>
   );
 }
---
+++ src/hooks/App_two.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./App.scss";
+import Input from "./components/input";
+
+function App_Two() {
+  const [name, setName] = useState("test");
+  const [income, setIncome] = useState("high");
+
+  const nameRef = useRef(null);
+  const ageRef = useRef(null);
+  const marriedRef = useRef(null);
+  const submitRef = useRef(null);
+
+  useEffect(() => {
+    nameRef.current.focus();
+  }, []);
+
+  function keyPressHandler(e) {
+    if (e.keyCode === 13) {
+      if (e.target.id === "nameInput") {
+        ageRef.current.focus();
+      }
+      if (e.target.id === "ageInput") {
+        marriedRef.current.focus();
+      }
+      if (e.target.id === "marriedInput") {
+        submitRef.current.focus();
+      }
+    }
+  }
+
+  function onClickHandle(e) {
+    console.log(e.target);
+  }
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleIncomeChange(e) {
+    setIncome(e.target.value);
+  }
+
+  function onSubmitHandle(e) {
+    console.log("State =>", name, income);
+    e.preventDefault();
+  }
+
+  const inputStyle = {
+    width: '400px',
+    height: '40px',
+    border: '1px solid #ccc',
+    outline: 0
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={onSubmitHandle}>
+        <div>
+          <label>Name:</label>
+          <input
+            value={name}
+            type="text"
+            onChange={handleNameChange}
+            placeholder="Enter name"
+          />
+        </div>
+
+        <div>
+          <label>Income:</label>
+          <select value={income} onChange={handleIncomeChange}>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+        </div>
+
+        <div>
+          <button type="submit">Save</button>
+        </div>
+      </form>
+
+      <div>
+        <label>Name:</label>
+        <Input ref={nameRef} id="nameInput" type="text" placeholder="Enter name" style={inputStyle} onKeyDown={keyPressHandler}></Input>
+      </div>
+
+      <div>
+        <label>Age:</label>
+        <Input ref={ageRef} id="ageInput" type="text" placeholder="Enter age" style={inputStyle} onKeyDown={keyPressHandler}></Input>
+      </div>
+
+      <div>
+        <label>Married:</label>
+        <Input ref={marriedRef} id="marriedInput" type="checkbox" onKeyDown={keyPressHandler}></Input>
+      </div>
+
+      <div>
+        <button ref={submitRef} onClick={onClickHandle} type="submit">Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App_Two;
